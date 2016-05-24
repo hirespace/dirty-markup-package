af53800bd3d9c75ba6f52d09abdea824f9622270
Thomas Owers <thomas@hirespace.com>
2016-05-24 13:06:52 +0100

    Initial commit

---
+++ src/dirtymarkup.js
@@ -0,0 +1,56 @@
+var http = require('http');
+var _ = require('lodash');
+
+var defaultRequestOptions = {
+    hostname: 'www.dirtymarkup.com',
+    port: 80,
+    path: '/src/ajax/dirty.ajax.php',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+};
+
+var defaultDirtymarkupOptions = {
+    code: '',
+    theme: 'dark',
+    'page-type': 'page',
+    'brace-style': 'collapse',
+    indent: 'none',
+    'indent-size': 4,
+    'print-margin': 80,
+    mode: 'html',
+    cmd: 'clean'
+};
+
+function getEncodedData(data) {
+    var parameters = [];
+    _.each(data, (value, key) => {
+        parameters.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
+    });
+    return parameters.join("&")
+}
+
+function sendRequest(dirtymarkupOptions, requestOptions, callback) {
+    var req = http.request(requestOptions, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', body => callback(JSON.parse(body)));
+    });
+    req.on('error', e => {
+        throw e;
+    });
+
+    req.write(getEncodedData(dirtymarkupOptions));
+    req.end();
+}
+
+export default function(options, callback) {
+    if (!callback) {
+        callback = options;
+        options = false;
+    }
+
+    var dmOptions = options ? _.merge(defaultDirtymarkupOptions, options) : defaultDirtymarkupOptions;
+    sendRequest(dmOptions, defaultRequestOptions, callback);
+}
+
---
+++ src/tester.js
@@ -0,0 +1,9 @@
+import dirtymarkup from './dirtymarkup';
+
+var options = {
+    code: "<a>Muahaahjdgssldjfjsbdfd</a>"
+};
+
+dirtymarkup(options, data => {
+    console.log(data);
+});
